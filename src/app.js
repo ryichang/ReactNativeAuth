7553937711e1812acbe78fe8abeb4fe5adc03449
ryichang <ryichang@gmail.com>
2016-10-03 14:02:45 -0700

    Added loggedIn state on app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAeNk8Ia5i4xvW-y-HGDYYcngNaDi_17-Y',
@@ -13,7 +15,15 @@ class App extends Component {
       databaseURL: 'https://react-7372d.firebaseio.com',
       storageBucket: 'react-7372d.appspot.com',
       messagingSenderId: '928789320841'
-  })
+  });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   render() {
--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
 
-cosnt Spinner = ({ size }) => {
+const Spinner = ({ size }) => {
   return(
       <View style={styles.spinnerStyle}>
         <ActivityIndicator size={size || 'large'} />
