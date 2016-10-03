2112ea4670e72a31b134767d1cd6a8287a365aa4
ryichang <ryichang@gmail.com>
2016-10-02 23:46:48 -0700

    Added State to TextInput

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAeNk8Ia5i4xvW-y-HGDYYcngNaDi_17-Y',
+      authDomain: 'react-7372d.firebaseapp.com',
+      databaseURL: 'https://react-7372d.firebaseio.com',
+      storageBucket: 'react-7372d.appspot.com',
+      messagingSenderId: '928789320841'
+  })
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication"/>
+        <LoginForm />
       </View>
     )
   };
