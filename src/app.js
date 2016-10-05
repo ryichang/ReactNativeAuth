d9e01e3b625edb040fd1245f5d7640b47b7c572c
ryichang <ryichang@gmail.com>
2016-10-04 17:38:54 -0700

    Logging a User out

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -26,11 +26,34 @@ class App extends Component {
     });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+
+    if (this.state.loggedIn) {
+      return (
+        <Button>
+          Log Out
+        </Button>
+      );
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication"/>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   };
